46297f9db632c1a5c0ee5673694bfac585bdb4d2
yuechuc <304723403@qq.com>
2023-01-12 00:48:36 +0800

    test

---
+++ src/main/java/com/yuechu/reegie/ReegieApplication.java
@@ -0,0 +1,13 @@
+package com.yuechu.reegie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReegieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReegieApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuechu/reegie/ReegieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuechu.reegie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReegieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
