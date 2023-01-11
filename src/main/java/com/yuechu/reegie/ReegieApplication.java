301bca3cb269daded0d027b9a667e6bc9e1e9f33
yuechuc <304723403@qq.com>
2023-01-12 01:32:03 +0800

    添加static文件

--- src/main/java/com/yuechu/reegie/ReegieApplication.java
+++ src/main/java/com/yuechu/reegie/ReegieApplication.java
@@ -1,13 +1,16 @@
 package com.yuechu.reegie;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class ReegieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReegieApplication.class, args);
+        log.info("outset sucess...");
     }
 
 }
