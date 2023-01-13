05c1fa5fc229d731d9d8da8b2ed706cfbb363e6a
yuechuc <304723403@qq.com>
2023-01-13 20:55:26 +0800

    完善登陆功能

--- src/main/java/com/yuechu/ReegieApplication.java
+++ src/main/java/com/yuechu/ReegieApplication.java
@@ -3,9 +3,11 @@ package com.yuechu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReegieApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yuechu/filter/LoginCheckFilter.java
@@ -0,0 +1,73 @@
+package com.yuechu.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.yuechu.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+public class LoginCheckFilter implements Filter {
+
+    //路径匹配器，支持通配符
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+//        1. 获取本次请求url
+        String requestURI = request.getRequestURI();
+        log.info("本次拦截url为：{}", requestURI);
+
+//        2. 判断本次请求是否需要处理
+        //不需要处理的请求路径
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        boolean check = check(urls, requestURI);
+
+
+//        3. 判断本次请求不需要处理，则直接放行
+        if (check) {
+            log.info("本次请求不需要处理：{}",requestURI);
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+//        4. 判断登陆状态，已登陆--》放行
+        if (request.getSession().getAttribute("employee") != null) {
+            log.info("已登陆，id为：{}",request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+//        5. 未登录则返回未登录结果,通过输出流方式向客户端页面响应数据
+        log.info("用户未登录");
+        response.getWriter().write(JSON.toJSONString(R.error("NOTLOGIN")));
+
+    }
+
+
+    public boolean check(String[] urls, String requestURI) {
+        for (String url : urls) {
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if (match) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
