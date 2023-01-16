e0b2e0da1dceafd4d25d3f4409f44f25afcabd0b
yuechuc <304723403@qq.com>
2023-01-17 04:57:22 +0800

    实现套餐信息分页查询功能

--- src/main/java/com/yuechu/controller/SetmealController.java
+++ src/main/java/com/yuechu/controller/SetmealController.java
@@ -2,18 +2,23 @@ package com.yuechu.controller;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yuechu.common.R;
 import com.yuechu.dto.SetmealDto;
 import com.yuechu.entity.Category;
 import com.yuechu.entity.Dish;
+import com.yuechu.entity.Setmeal;
 import com.yuechu.entity.SetmealDish;
+import com.yuechu.service.CategoryService;
 import com.yuechu.service.SetmealDishService;
 import com.yuechu.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/setmeal")
@@ -26,12 +31,48 @@ public class SetmealController {
     @Autowired
     private SetmealDishService setmealDishService;
 
+    @Autowired
+    private CategoryService categoryService;
 
     @PostMapping
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info(setmealDto.toString());
         setmealService.saveWithDish(setmealDto);
         return R.success("success");
+    }
+
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String name){
+
+        Page<Setmeal> setmealPage = new Page<>(page,pageSize);
+
+        Page<SetmealDto> setmealDtoPage = new Page<>();
+        BeanUtils.copyProperties(setmealPage,setmealDtoPage,"records");
+
+        LambdaQueryWrapper<Setmeal> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.like(name!=null,Setmeal::getName,name);
+        queryWrapper.orderByDesc(Setmeal::getUpdateTime);
+
+        setmealService.page(setmealPage,queryWrapper);
+
+        List<Setmeal> records = setmealPage.getRecords();
+        log.info(records.toString());
+
+        List<SetmealDto> list = records.stream().map((item)->{
+            SetmealDto setmealDto = new SetmealDto();
+            BeanUtils.copyProperties(item,setmealDto);
+
+            Long categoryId = item.getCategoryId();
+            Category category = categoryService.getById(categoryId);
+            if (category!=null){
+                String categoryName = category.getName();
+                setmealDto.setCategoryName(categoryName);
+            }
+            return setmealDto;
+        }).collect(Collectors.toList());
+
+        setmealDtoPage.setRecords(list);
+        return R.success(setmealDtoPage);
     }
 
 }
